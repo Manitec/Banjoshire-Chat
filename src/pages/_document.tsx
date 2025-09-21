7fbec84f2ab25a84905e631e47a5aa6739ecdfa8
Joe <95106688+Manitec@users.noreply.github.com>
2025-09-21 04:55:04 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,7 @@ export default function Document() {
         <body>
           <Main />
           <NextScript />
+          <script>navigator.serviceWorker.register("public/sw.js")</script>
         </body>
       </Html>
     </>
